22f0a3f20e737623a2c1ac532011ba6fd38db140
Vika Vika <vika2108991@gmail.com>
2022-03-10 14:10:30 +0400

    fix method "create a new product", add method "delete all"

--- application.py
+++ application.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 import os
 from pymongo import MongoClient
+import json
+from bson import ObjectId, json_util
+from bson.json_util import dumps
 
 
 app = Flask(__name__)
@@ -10,62 +13,67 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
 
-
+# Просмотреть содержимое коллекции
 @app.route('/')
 def index():
     products = db.product.find()
     
+    # item = {}
+    # data = []
+    # for product in products:
+    #     item = {
+    #         'id': str(product['_id']),
+    #         'name': product['name'],
+    #         'description': product['description'],
+    #         'options': product['options']
+    #     }
+    #     data.append(item)
+
+    data = []
+    for product in products:
+        product['_id'] = str(product['_id'])  
+        data.append(product)
+
+    return jsonify(
+        status=True,
+        message='This is a "product" collection of MongoDB',
+        products=data
+    )
+
+
+# work!
+# curl -X POST http://localhost:5000/add_product -d '{"name": "product0", "description": "123", "options": {"english": "yes", "german": "no"}}'
+# Создать новый товар
+@app.route('/add_product', methods=['POST'])
+def create_a_new_product():
+    data = request.get_json(force=True)
+    pr = db.product.insert_one(data)
+
+    return jsonify(
+        status=True,
+        message='A new product created successfully',
+        id=str(pr.inserted_id),
+    ), 201
+
+
+# Получить список названий товаров, с возможностью фильтрации по: a) названию
+@app.route('/get_names')
+def get_names():
+    products = db.product.find()
+
     item = {}
     data = []
     for product in products:
         item = {
             'id': str(product['_id']),
-            'name': product['name'],
-            'description': product['description'],
-            'options': product['options']
+            'name': product['name']
         }
         data.append(item)
 
     return jsonify(
         status=True,
-        message='Welcome to the Dockerized Flask MongoDB app!',
-        products=data
+        data=data
     )
-
-
-# Создать новый товар
-@app.route('/create_a_new_product', methods=['POST'])
-def create_a_new_product():
-    data = request.get_json(force=True)
-    item = {
-        'product': data['product']
-    }
-    db.product.insert_one(item)
-
-    return jsonify(
-        status=True,
-        message='A new product created successfully'
-    ), 201
-
-
-# Получить список названий товаров, с возможностью фильтрации по: a) названию
-# @app.route('/get_names')
-# def get_names():
-#     products = db.product.find()
-
-#     item = {}
-#     data = []
-#     for product in products:
-#         item = {
-#             'id': str(product['_id']),
-#             'name': product['name']
-#         }
-#         data.append(item)
-
-#     return jsonify(
-#         status=True,
-#         data=data
-#     )
 
 
 # # b) выбранному параметру и его значению
@@ -90,10 +98,11 @@ def create_a_new_product():
 #     )
 
 
+# curl -X GET http://localhost:5000/products/123123123
 # Получить детали товара по ID
-@app.route('/get_product/', methods=['POST'])
-def insert_one(productId):
-    products = db.product.find_one({"_id": productId})
+@app.route('/products/<product_id>', methods=['GET'])
+def insert_one(product_id):
+    products = db.product.find_one({"_id": product_id})
     
     item = {}
     data = []
@@ -110,6 +119,42 @@ def insert_one(productId):
         status=True,
         data=data
     )
+
+
+# curl -X DELETE http://localhost:5000/delete_one/6228ba9c7667c0cf85c8bb67
+# Удалить товар по ID
+@app.route('/delete_one/<product_id>', methods=['DELETE'])
+def delete_one_product(product_id):
+    item = {
+        '_id': product_id
+    }
+    db.product.delete_one(item)
+    # db.products.delete_many(item)
+    
+    # products = db.product.find()
+    # data = []
+    # for product in products:
+    #   product['_id'] = str(product['_id'])  
+    #   data.append(product)
+
+    return jsonify(
+        status=True,
+        message='A product deleted successfully',
+        # data=data
+    ), 200
+
+
+# work!
+# curl -X DELETE http://localhost:5000/delete_all
+# Очистить коллекцию
+@app.route('/delete_all', methods=['DELETE'])
+def delete_all():
+    db.product.drop()
+
+    return jsonify(
+        status=True,
+        message='All docs in collection deleted successfully'
+    ), 200
 
 
 if __name__ == "__main__":
