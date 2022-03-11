d0be47474fad7a9a651e56d905a52caf9fe20ab5
Vika Vika <vika2108991@gmail.com>
2022-03-11 18:39:00 +0400

    cosmetic of application.py file

--- application.py
+++ application.py
@@ -14,39 +14,53 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
 
-# done!
-# Просмотреть содержимое коллекции
-@app.route('/')
-def index():
-    products = db.product.find()
-    
-    # ---The first version of the collection view---
-    item = {}
-    data = []
-    for product in products:
-        item = {
-            'id': str(product['_id']),
-            'name': product['name'],
-            'description': product['description'],
-            'options': product['options']
-        }
-        data.append(item)
 
-    # ---The second version of the collection view---
+@app.route('/', methods=['GET'])
+def collection_view():
+    products = db.product.find()
+
+    # # ---The first version of the collection view---
+    # item = {}
     # data = []
     # for product in products:
-    #     product['_id'] = str(product['_id'])  
-    #     data.append(product)
+    #     item = {
+    #         'id': str(product['_id']),
+    #         'name': product['name'],
+    #         'description': product['description'],
+    #         'options': product['options']
+    #     }
+    #     data.append(item)
+
+    # ---The second version of the collection view---
+    data = []
+    for product in products:
+        product['_id'] = str(product['_id'])
+        data.append(product)
 
     return jsonify(
-        message='This is a "product" collection of MongoDB',
+        message='This is a collection of MongoDB',
         products=data
     )
 
 
-# done!
-# curl -X POST http://localhost:5000/add_product -d '{"name": "product4", "description": "123444", "options": [{"english": "yesss", "german": "nooo"}]}'
-# Создать новый товар
+@app.route('/add_collection', methods=['POST'])
+def create_a_collection():
+    collection = []
+    collection = [{"name": "product0", "description": "123", "options": [{"a": "yes", "b": "no"}]},
+                  {"name": "product1", "description": "1234",
+                      "options": [{"a": "yess", "b": "noo"}]},
+                  {"name": "product1", "description": "12345",
+                   "options": [{"a": "yesss", "b": "nooo"}]},
+                  {"name": "product2", "description": "123456",
+                   "options": [{"a": "yessss", "b": "noooo"}]},
+                  {"name": "product3", "description": "123456", "options": [{"a": "yesssss", "b": "nooooo"}]}]
+    db.product.insert_many(collection)
+
+    return jsonify(
+        message='A collection created successfully',
+    ), 201
+
+
 @app.route('/add_product', methods=['POST'])
 def create_a_new_product():
     data = request.get_json(force=True)
@@ -58,41 +72,35 @@ def create_a_new_product():
     ), 201
 
 
-# done!
-# a) Получить список названий товаров, с возможностью фильтрации по названию
-    # Если надо вывести названия всех товаров коллекции, то вам нужен запрос: curl -X GET http://localhost:5000/by_name/_ (где product_name == "_")
-    # Если надо проверить наличие конкретного товара, то вам нужен запрос: curl -X GET http://localhost:5000/by_name/product0 (где product0 - название вашего товара)
-        # Если надо вывести еще и ID товаров, то из строк 69 и 71 нужно убрать '"_id":0' и раскомментировать строку 75
+# If you want to output the product IDs as well, you need to remove '"_id": 0' from lines 79 and 82 and uncomment line 86
 @app.route('/by_name/<product_name>', methods=['GET'])
 def get_product_by_name(product_name):
-    if product_name=='_':
-        products = db.product.find({},{"name":1,"_id":0})
+    if product_name == '_':
+        products = db.product.find({}, {"name": 1, "_id": 0})
     else:
-        products = db.product.find({"name": product_name},{"name":1,"_id":0})
-    
+        products = db.product.find(
+            {"name": product_name}, {"name": 1, "_id": 0})
+
     data = []
     for product in products:
-        # product['_id'] = str(product['_id'])  
+        # product['_id'] = str(product['_id'])
         data.append(product)
 
     return jsonify(
         data=data
     )
 
-# done!
-# curl -X GET http://localhost:5000/by_parameter/name/product0 
-# curl -X GET http://localhost:5000/by_parameter/_id/622b3d98f8140baa6b613cf1 
-# curl -X GET http://localhost:5000/by_parameter/description/1234 
-# curl -X GET http://localhost:5000/by_parameter/english/yesss
-# b) Получить список названий товаров, с возможностью фильтрации по выбранному параметру и его значению
+
 @app.route('/by_parameter/<parameter>/<value>', methods=['GET'])
 def get_product_by_parameter(parameter, value):
-    if parameter=='_id':
-        products = db.product.find({"_id": ObjectId(value)}, {"name":1,"_id":0})
-    elif parameter=='english' or parameter=='german':
-        products = db.product.find({'options': {'$elemMatch': {parameter: value}}}, {"name":1,"_id":0})
+    if parameter == '_id':
+        products = db.product.find(
+            {"_id": ObjectId(value)}, {"name": 1, "_id": 0})
+    elif parameter == 'english' or parameter == 'german':
+        products = db.product.find(
+            {'options': {'$elemMatch': {parameter: value}}}, {"name": 1, "_id": 0})
     else:
-        products = db.product.find({parameter: value}, {"name":1,"_id":0})
+        products = db.product.find({parameter: value}, {"name": 1, "_id": 0})
 
     data = []
     for product in products:
@@ -103,16 +111,13 @@ def get_product_by_parameter(parameter, value):
     )
 
 
-# done!
-# curl -X GET http://localhost:5000/products/6229cc91ad7f7f4a53ce9804
-# Получить детали товара по ID
 @app.route('/product/<product_id>', methods=['GET'])
-def insert_one(product_id):
+def get_product_details_by_id(product_id):
     products = db.product.find({"_id": ObjectId(product_id)})
 
     data = []
     for product in products:
-        product['_id'] = str(product['_id'])  
+        product['_id'] = str(product['_id'])
         data.append(product)
 
     return jsonify(
@@ -120,39 +125,19 @@ def insert_one(product_id):
     )
 
 
-# done!
-# curl -X DELETE http://localhost:5000/delete_one/6229cc70ad7f7f4a53ce9803
-# Удалить товар по ID
 @app.route('/delete_one/<product_id>', methods=['DELETE'])
 def delete_one_product(product_id):
-    # products = db.product.find()
-    # data_before = []
-    # for product in products:
-    #     product['_id'] = str(product['_id'])  
-    #     data_before.append(product)
-    
     item = {
         '_id': ObjectId(product_id)
     }
     db.product.delete_one(item)
     # db.products.delete_many(item)
 
-    # products = db.product.find()
-    # data_after = []
-    # for product in products:
-    #     product['_id'] = str(product['_id'])  
-    #     data_after.append(product)
-
     return jsonify(
         message='A product deleted successfully',
-        # data_before=data_before,
-        # data_after=data_after
     ), 200
 
 
-# done!
-# curl -X DELETE http://localhost:5000/delete_all
-# Очистить коллекцию
 @app.route('/delete_all', methods=['DELETE'])
 def delete_all():
     db.product.drop()
